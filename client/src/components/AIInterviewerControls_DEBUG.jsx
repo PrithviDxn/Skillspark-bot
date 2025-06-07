6e539868e5e46d776968e639c8c030800a9f6812
PrithviDxn <prithvi.daniel@btech.christuniversity.in>
2025-06-07 11:41:23 +0530

    video

---
+++ client/src/components/AIInterviewerControls_DEBUG.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+export default function AIInterviewerControls_DEBUG() {
+  console.log('DEBUG COMPONENT RENDERED');
+  return <div style={{color: 'red', fontWeight: 'bold', fontSize: 24}}>DEBUG COMPONENT</div>;
+} 
