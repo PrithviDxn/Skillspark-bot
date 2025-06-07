7f81fc77ce02e2fb75f0197a50c3fd4216a6ed58
PrithviDxn <prithvi.daniel@btech.christuniversity.in>
2025-06-07 10:20:35 +0530

    buid

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+console.log('!!! SKILLSPARK LATEST BUILD ' + new Date().toISOString() + ' !!!');
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
